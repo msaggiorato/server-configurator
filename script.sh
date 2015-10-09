4cc6557f82a42ab89e4985dcdbbbc2a9bd6c34bc
Matias Saggiorato <matiassaggiorato@gmail.com>
2015-10-09 03:18:00 -0300

    Added install of htop and ajenti

--- script.sh
+++ script.sh
@@ -5,11 +5,15 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo ':: Setting up SWAP'
+
 fallocate -l 2G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
-
 echo "vm.swappiness=10" >> /etc/sysctl.d/99-better-swap.conf
 echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.d/99-better-swap.conf
+
+echo ':: Installing tools'
+apt-get install -y htop
+wget -O- https://raw.github.com/ajenti/ajenti/1.x/scripts/install-ubuntu.sh | sudo sh
